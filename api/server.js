5afe9a61b34b2a146afe6e22795eca7764d956f6
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-01-05 20:40:49 -0300

    Server.js Configuration

---
+++ api/server.js
@@ -0,0 +1,62 @@
+// Pacotes
+const compression = require('compression');
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+
+// Start
+const app = express();
+
+// Ambiente
+const isProduction = process.env.NODE_ENV === 'production';
+const PORT = process.env.PORT || 3000;
+
+// Arquivos Estaticos
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/public/images', express.static(__dirname + '/public/images'));
+
+// Setup MongoDB
+const dbs = require('./config/database');
+const dbURI = isProduction ? dbs.dbProduction : dbs.dbTest;
+mongoose.connect(dbURI, { useNewUrlParser: true });
+
+// Setup EJS
+app.set('view engine', 'ejs');
+
+// Configuracoes
+if (!isProduction) app.use(morgan('dev'));
+app.use(cors());
+app.disable('x-powered-by');
+app.use(compression());
+
+// Setup Body Parser
+app.use(bodyParser.urlencoded({ extended: false, limit: 1.5 * 1024 * 1024 }));
+app.use(bodyParser.json({ limit: 1.5 * 1024 * 1024 }));
+
+// Models
+require('./models');
+// Rotas
+app.use('/', require('./routes'));
+
+// 404 - Rota
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Rota - 422 / 500 / 401
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  if (err.status !== 404) console.warn('Error: ', err.message, new Date());
+  res.json({ erros: { message: err.message, status: err.status } });
+});
+
+// Escutar
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Rodando na //localhost:${PORT}`);
+});
