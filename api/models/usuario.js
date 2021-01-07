6ac465fd48be00e83d8b0901b58f44d90143032c
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-01-07 19:33:44 -0300

    'User' Model Configuration II

--- api/models/usuario.js
+++ api/models/usuario.js
@@ -40,3 +40,61 @@ const UsuarioSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
+
+UsuarioSchema.plugin(uniqueValidator, { message: 'já está sendo utilizado' });
+
+UsuarioSchema.methods.setSenha = function (password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto
+    .pbkdf2Sync(password, this.salt, 10000, 512, 'sha512')
+    .toString('hex');
+};
+
+UsuarioSchema.methods.validarSenha = function (password) {
+  const hash = crypto
+    .pbkdf2Sync(password, this.salt, 10000, 512, 'sha512')
+    .toString('hex');
+  return hash === this.hash;
+};
+
+UsuarioSchema.methods.gerarToken = function () {
+  const hoje = new Date();
+  const exp = new Date(today);
+  exp.setDate(today.getDate() + 15);
+
+  return jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+      nome: this.nome,
+      exp: parseFloat(exp.getTime() / 1000, 10),
+    },
+    secret,
+  );
+};
+
+UsuarioSchema.methods.enviarAuthJSON = function () {
+  return {
+    _id: this._id,
+    nome: this.nome,
+    email: this.email,
+    loja: this.loja,
+    role: this.permissao,
+    token: this.gerarToken(),
+  };
+};
+
+// Recuperação
+UsuarioSchema.methods.criarTokenRecuperacaoSenha = function () {
+  this.recovery = {};
+  this.recovery.token = crypto.randomBytes(16).toString('hex');
+  this.recovery.date = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+  return this.recovery;
+};
+
+UsuarioSchema.methods.finalizarTokenRecuperacaoSenha = function () {
+  this.recovery = { token: null, date: null };
+  return this.recovery;
+};
+
+module.exports = mongoose.model('Usuario', UsuarioSchema);
