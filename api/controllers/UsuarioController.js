adccb0c06163530de6804dd79493bd46b67792f0
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-01-08 19:12:00 -0300

    Controller and Routes 3

--- api/controllers/UsuarioController.js
+++ api/controllers/UsuarioController.js
@@ -37,6 +37,11 @@ class UsuarioController {
   store(req, res, next) {
     const { nome, email, password } = req.body;
 
+    if (!nome || !email || !password)
+      return res
+        .status(422)
+        .json({ errors: 'Preencha todos os campos de cadastro.' });
+
     const usuario = new Usuario({ nome, email });
     usuario.setSenha(password);
 
@@ -103,5 +108,100 @@ class UsuarioController {
         return res.json({ usuario: usuario.enviarAuthJSON() });
       })
       .catch(next);
+  }
+
+  // Recovery
+
+  // Get /recuperar-senha
+  showRecovery(req, res, next) {
+    return res.render('recovery', { error: null, sucess: null });
+  }
+
+  // Post /recuperar-senha
+  createRecovery(req, res, next) {
+    const { email } = req.body;
+    if (!email)
+      return res.render('recovery', {
+        error: 'Preencha com o seu email',
+        success: null,
+      });
+
+    Usuario.findOne({ email })
+      .then((usuario) => {
+        if (!usuario)
+          return res.render('recovery', {
+            error: 'Não existe usuário com este email',
+            success: null,
+          });
+        const recoveryData = usuario.criarTokenRecuperacaoSenha();
+        return usuario
+          .save()
+          .then(() => {
+            return res.render('recovery', { error: null, success: true });
+          })
+          .catch(next);
+      })
+      .catch(next);
+  }
+
+  // Get /senha-recuperada
+  showCompleteRecovery(req, res, next) {
+    if (!req.query.token)
+      return res.render('recovery', {
+        error: 'Token não identificado',
+        success: null,
+      });
+    Usuario.findOne({
+      'recovery.token': req.query.token,
+    })
+      .then((usuario) => {
+        if (!usuario)
+          return res.render('recovery', {
+            error: 'Não existe usuário com este token',
+            success: null,
+          });
+        if (new Date(usuario.recovery.date) < new Date())
+          return res.render('recovery', {
+            error: 'Token expirado. Tente novamente.',
+            success: null,
+          });
+        return res.render('recovery/store', {
+          error: null,
+          success: null,
+          token: req.query.token,
+        });
+      })
+      .catch(next);
+  }
+
+  // Post /senha-recuperada
+  completeRecovery(req, res, next) {
+    const { token, password } = req.body;
+    if (!token || !password)
+      return res.render('recovery/store', {
+        error: 'Preencha novamente com sua nova senha',
+        success: null,
+        token: token,
+      });
+    Usuario.findOne({ 'recovery.token': token }).then((usuario) => {
+      if (!usuario)
+        return res.render('recovery', {
+          error: 'Usuário não identificado',
+          success: null,
+        });
+
+      usuario.finalizarTokenRecuperacaoSenha();
+      usuario.setSenha(password);
+      return usuario
+        .save()
+        .then(() => {
+          return res.render('recovery/store', {
+            error: null,
+            success: 'Senha alterada com sucesso. Tente novamente fazer login.',
+            token: null,
+          });
+        })
+        .catch(next);
+    });
   }
 }
