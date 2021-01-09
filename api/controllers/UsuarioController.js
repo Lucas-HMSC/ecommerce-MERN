3ae5df0652b62fab7b3917ee311702d2d4db3e63
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-01-09 10:31:57 -0300

    Create Helper 'Email Recovery'

--- api/controllers/UsuarioController.js
+++ api/controllers/UsuarioController.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Usuario = mongoose.model('Usuario');
-//const enviarEmailRecovery = require('../helpers/email-recovery');
+const enviarEmailRecovery = require('../helpers/email-recovery');
 
 class UsuarioController {
   // Get /
@@ -137,7 +137,13 @@ class UsuarioController {
         return usuario
           .save()
           .then(() => {
-            return res.render('recovery', { error: null, success: true });
+            enviarEmailRecovery(
+              { usuario, recovery: recoveryData },
+              ((error = null),
+              (success = null) => {
+                return res.render('recovery', { error, success });
+              }),
+            );
           })
           .catch(next);
       })
