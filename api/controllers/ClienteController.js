d424e9fd47ed2046a48ff356a68a9667d80ecfd6
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-01-15 18:28:39 -0300

    Create Controller for Admin

---
+++ api/controllers/ClienteController.js
@@ -0,0 +1,96 @@
+const mongoose = require('mongoose');
+
+const Cliente = mongoose.model('Cliente');
+const Usuario = mongoose.model('Usuario');
+
+class ClienteController {
+  /*
+   * ADMINISTRADOR
+   */
+
+  // Get /index
+  async index(req, res, next) {
+    try {
+      const offset = Number(req.query.offset) || 0;
+      const limit = Number(req.query.limit) || 30;
+      const clientes = await Cliente.paginate(
+        { loja: req.query.loja },
+        { offset, limit, populate: 'usuario' },
+      );
+      return res.send({ clientes });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  // Get /search/:search/pedidos
+  searchPedidos(req, res, next) {
+    return res.status(400).send({ error: 'Em desenvolvimento.' });
+  }
+
+  // Get /search/:search
+  async search(req, res, next) {
+    const offset = Number(req.query.offset) || 0;
+    const limit = Number(req.query.limit) || 30;
+    const search = new RegExp(req.params.search, 'i');
+    try {
+      const clientes = await Cliente.paginate(
+        { loja: req.query.loja, nome: { $regex: search } },
+        { offset, limit, populate: 'usuario' },
+      );
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  // Get /admin/:id
+  async showAdmin(req, res, next) {
+    try {
+      const cliente = await Cliente.findOne({
+        _id: req.params.id,
+        loja: req.query.loja,
+      }).populate('usuario');
+      return res.send({ cliente });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  // Get /admin/:id/pedidos
+  showPedidosCliente(req, res, next) {
+    return res.status(400).send({ error: 'Em desenvovlimento.' });
+  }
+
+  // Put /admin/:id
+  async updateAdmin(req, res, next) {
+    const {
+      nome,
+      cof,
+      email,
+      telefones,
+      endereco,
+      dataDeNascimento,
+    } = req.body;
+    try {
+      const Cliente = await Cliente.findById(req.params.id).populate('usuario');
+      if (nome) {
+        cliente.usuario.nome = nome;
+        cliente.nome = nome;
+      }
+      if (email) cliente.usuario.email = email;
+      if (telefones) cliente.usuario.telefones = telefones;
+      if (endereco) cliente.usuario.endereco = endereco;
+      if (dataDeNascimento) cliente.dataDeNascimento = dataDeNascimento;
+      await cliente.save();
+      return res.send({ cliente });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  /*
+   * CLIENTE
+   */
+}
+
+module.exports = ClienteController;
