5e0a15db69659a02af98cbe07ab40e7510d82076
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-01-17 11:11:50 -0300

    Create 'Categoria' Validation

--- api/routes/api/v1/categorias.js
+++ api/routes/api/v1/categorias.js
@@ -13,26 +13,41 @@ const {
 
 const categoriaController = new CategoriaController();
 
-router.get('/', categoriaController.index);
-router.get('/disponiveis', categoriaController.indexDisponiveis);
-router.get('/:id', categoriaController.show);
+router.get(
+  '/',
+  Validation(CategoriaValidation.index),
+  categoriaController.index,
+);
+router.get(
+  '/disponiveis',
+  Validation(CategoriaValidation.indexDisponiveis),
+  categoriaController.indexDisponiveis,
+);
+router.get(
+  '/:id',
+  Validation(CategoriaValidation.show),
+  categoriaController.show,
+);
 
 router.post(
   '/',
   auth.required,
   LojaValidation.admin,
+  Validation(CategoriaValidation.store),
   categoriaController.store,
 );
 router.put(
   '/:id',
   auth.required,
   LojaValidation.admin,
+  Validation(CategoriaValidation.update),
   categoriaController.update,
 );
 router.delete(
   '/:id',
   auth.required,
   LojaValidation.admin,
+  Validation(CategoriaValidation.remove),
   categoriaController.remove,
 );
 
