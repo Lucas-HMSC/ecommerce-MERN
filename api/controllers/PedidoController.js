d39fb62b879a28a2e6945dcb84677e16063d595e
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-01-20 21:11:33 -0300

    Create 'Correios' Integration

--- api/controllers/PedidoController.js
+++ api/controllers/PedidoController.js
@@ -8,6 +8,8 @@ const Entrega = mongoose.model('Entrega');
 const Cliente = mongoose.model('Cliente');
 const RegistroPedido = mongoose.model('RegistroPedido');
 
+const { calcularFrete } = require('./integracoes/correios');
+
 const CarrinhoValidation = require('./validacoes/carrinhoValidation');
 
 class PedidoController {
@@ -164,6 +166,7 @@ class PedidoController {
         }),
       );
       const registros = await RegistroPedido.find({ pedido: pedido._id });
+
       return res.send({ pedido, registros });
     } catch (e) {
       next(e);
