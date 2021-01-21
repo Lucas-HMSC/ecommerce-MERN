9ce7b430f03f0846d30af3b6e04420b95b6ae181
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-01-21 20:44:45 -0300

    Update Models

--- api/models/entrega.js
+++ api/models/entrega.js
@@ -23,6 +23,18 @@ const EntregaSchema = Schema(
       type: Number,
       required: true,
     },
+    endereco: {
+      type: {
+        local: { type: String, required: true },
+        numero: { type: String, required: true },
+        complemento: { type: String },
+        bairro: { type: String, required: true },
+        cidade: { type: String, required: true },
+        estado: { type: String, required: true },
+        CEP: { type: String, required: true },
+      },
+      required: true,
+    },
     pedido: {
       type: Schema.Types.ObjectId,
       ref: 'Pedido',
--- api/models/pagamento.js
+++ api/models/pagamento.js
@@ -12,12 +12,39 @@ const PagamentoSchema = Schema(
       type: String,
       required: true,
     },
-    parcelado: {
-      type: Object,
+    parcelas: {
+      type: Number,
+      default: 1,
     },
     status: {
       type: String,
       required: true,
+    },
+    endereco: {
+      type: {
+        local: { type: String, required: true },
+        numero: { type: String, required: true },
+        complemento: { type: String },
+        bairro: { type: String, required: true },
+        cidade: { type: String, required: true },
+        estado: { type: String, required: true },
+        CEP: { type: String, required: true },
+      },
+      required: true,
+    },
+    cartao: {
+      type: {
+        nomeCompleto: { type: String, required: true },
+        codigoArea: { type: String, required: true },
+        telefone: { type: String, required: true },
+        dataDeNascimento: { type: String, required: true },
+        credit_card_token: { type: String, required: true },
+        cpf: { type: String, required: true },
+      },
+    },
+    enderecoEntregaIgualCobranca: {
+      type: Boolean,
+      default: true,
     },
     pedido: {
       type: Schema.Types.ObjectId,
@@ -30,7 +57,10 @@ const PagamentoSchema = Schema(
       required: true,
     },
     payload: {
-      type: Object,
+      type: Array,
+    },
+    pagSeguroCode: {
+      type: String,
     },
   },
   { timestamps: true },
