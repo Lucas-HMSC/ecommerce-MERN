60090ebea0ad8a2bdc2b315e993b3e28c8594107
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-01-20 21:41:42 -0300

    Create EntregaController.js

---
+++ api/controllers/EntregaController.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose');
+const { calcularFrete } = require('./integracoes/correios');
+
+const Entrega = mongoose.model('Entrega');
+const Produto = mongoose.model('Produto');
+const Variacao = mongoose.model('Variacao');
+const RegistroPedido = mongoose.model('RegistroPedido');
+
+class EntregaController {
+  // Get /:id
+  async show(req, res, next) {
+    try {
+      const entrega = await Entrega.findOne({
+        _id: req.params.id,
+        loja: req.query.loja,
+      });
+      const registros = await RegistroPedido.find({
+        pedido: entrega.pedido,
+        tipo: 'entrega',
+      });
+      return res.send({ entrega, registros });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  // Put /:id
+  async update(req, res, next) {
+    const { situacao, CodigoRastreamento } = req.body;
+    const { loja } = req.query;
+    try {
+      const entrega = await Entrega.findOne({ loja, _id: req.params.id });
+
+      if (situacao) entrega.situacao = situacao;
+      if (CodigoRastreamento) entrega.CodigoRastreamento = CodigoRastreamento;
+
+      const registroPedido = new RegistroPedido({
+        pedido: entrega.pedido,
+        tipo: 'entrega',
+        situacao,
+        payload: req.body,
+      });
+      await registroPedido.save();
+      // Enviar email de aviso para o cliente - aviso de atualizacao na entrega
+
+      await entrega.save();
+      return res.send({ entrega });
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  // Post /calcular
+  async calcular(req, res, next) {
+    const { cep, carrinho } = req.body;
+    try {
+      const _carrinho = await Promisse.all(
+        carrinho.map(async (item) => {
+          item.produto = await Produto.findById(item.produto);
+          item.variacao = await Variacao.findById(item.variacao);
+          return item;
+        }),
+      );
+      const resultados = await calcularFrete(cep, _carrinho);
+      return res.send({ resultados });
+    } catch (e) {
+      next(e);
+    }
+  }
+}
+
+module.exports = EntregaController;
