9043fd71f102844d5f9cfd53f8ff2d00a2a63d03
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-01-20 21:51:57 -0300

    Create 'Entrega' Validation

---
+++ api/controllers/validacoes/entregaValidation.js
@@ -0,0 +1,41 @@
+const Joi = require('joi');
+
+const EntregaValidation = {
+  show: {
+    params: {
+      id: Joi.string().alphanum().length(24).required(),
+    },
+    query: {
+      loja: Joi.string().alphanum().length(24).required(),
+    },
+  },
+  update: {
+    params: {
+      id: Joi.string().alphanum().length(24).required(),
+    },
+    query: {
+      loja: Joi.string().alphanum().length(24).required(),
+    },
+    body: {
+      situacao: Joi.string().optional(),
+      codigoRastreamento: Joi.string().optional(),
+    },
+  },
+  calcular: {
+    body: {
+      cep: Joi.string().required(),
+      carrinho: Joi.array()
+        .items(
+          Joi.object({
+            produto: Joi.string().alphanum().length(24).required(),
+            variacao: Joi.string().alphanum().length(24).required(),
+            precoUnitario: Joi.number().optional(),
+            quantidade: Joi.number().optional(),
+          }),
+        )
+        .required(),
+    },
+  },
+};
+
+module.exports = { EntregaValidation };
--- api/routes/api/v1/entregas.js
+++ api/routes/api/v1/entregas.js
@@ -5,17 +5,31 @@ const EntregaController = require('../../../controllers/EntregaController');
 const {
   LojaValidation,
 } = require('../../../controllers/validacoes/lojaValidation');
+const Validation = require('express-validation');
+const {
+  EntregaValidation,
+} = require('../../../controllers/validacoes/entregaValidation');
 const auth = require('../../auth');
 
 const entregaController = new EntregaController();
 
-router.get('/:id', auth.required, entregaController.show);
+router.get(
+  '/:id',
+  auth.required,
+  Validation(EntregaValidation.show),
+  entregaController.show,
+);
 router.put(
   '/:id',
   auth.required,
   LojaValidation.admin,
+  Validation(EntregaValidation.update),
   entregaController.update,
 );
-router.post('/calcular', entregaController.calcular);
+router.post(
+  '/calcular',
+  Validation(EntregaValidation.calcular),
+  entregaController.calcular,
+);
 
 module.exports = router;
