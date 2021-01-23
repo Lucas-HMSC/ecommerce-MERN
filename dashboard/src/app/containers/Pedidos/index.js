1614361380927ca22afc5843ecec242e2f19ac81
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-01-23 11:03:18 -0300

    Initiated Routes

---
+++ dashboard/src/app/containers/Pedidos/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Titulo from '../../components/Texto/Titulo';
+
+class Pedidos extends Component {
+  render() {
+    return (
+      <div className="Pedidos">
+        <Titulo tipo="h1" titulo="Pedidos" />
+      </div>
+    );
+  }
+}
+
+export default Pedidos;
--- dashboard/src/app/index.js
+++ dashboard/src/app/index.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import { HashRouter as Router, Route } from 'react-router-dom';
+
+import Pedidos from './containers/Pedidos';
+
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="App">Dashboard</div>
+        <Router>
+          <div className="App">
+            <Route path={'/'} exact component={Pedidos} />
+          </div>
+        </Router>
       </Provider>
     );
   }
