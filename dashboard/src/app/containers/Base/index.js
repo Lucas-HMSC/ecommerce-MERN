5749c36368eac27142fe7326721e97ef6bc61bb6
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-01-23 12:12:17 -0300

    Create Structure of Menu

--- dashboard/src/app/containers/Base/index.js
+++ dashboard/src/app/containers/Base/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 
+const BarraTopo = () => (
+  <div className="flex horizontal full-width">
+    <div className="flex-1 flex flex-start">
+      <p>Ver Loja</p>
+    </div>
+    <div className="flex-1 flex flex-end">
+      <p>Sair</p>
+    </div>
+  </div>
+);
+
 class Dashboard extends React.Component {
   render() {
     return (
-      <div className="flex horizontal">
+      <div className="flex horizontal full-height">
         <div className="flex vertical">
           <p>Menu</p>
         </div>
-        <div className="flex vertical">
-          <div className="flex horizontal">Barra Topo</div>
-          <div className="flex">{this.props.children}</div>
+        <div className="flex vertical full-width">
+          <div className="flex horizontal">
+            <BarraTopo />
+          </div>
+          <div className="flex full-height">{this.props.children}</div>
         </div>
       </div>
     );
