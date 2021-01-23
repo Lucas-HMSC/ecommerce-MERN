21394ddf5370e15ef5bb648b361fc0a5bfc926ac
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-01-22 21:36:47 -0300

    Create Notification Messages

--- api/controllers/EmailController.js
+++ api/controllers/EmailController.js
@@ -27,13 +27,71 @@ const _send = ({ subject, emails, message }, cb = null) => {
 };
 
 // Novo Pedido
-const enviarNovoPedido = ({ usuario, pedido }) => {};
+const enviarNovoPedido = ({ usuario, pedido }) => {
+  const message = `
+    <h1 style="text-align: center;">Pedido Recebido</h1>
+    <br />
+    <p>O pedido realizado hoje, no dia ${moment(pedido.createdAt).format(
+      'DD/MM/YYYY',
+    )}, foi recebido com sucesos.</p>
+    <br />
+    <a href="${loja}">Acesse a loja para saber mais.</a>
+    <br /><br />
+    <p>Atenciosamente,</p>
+    <p>Equipe - Loja TI</p>
+  `;
+  _send({
+    subject: 'Pedido Recebido - Loja TI',
+    emails: usuario.email,
+    message,
+  });
+};
 
 // Pedido Cancelado
-const cancelarPedido = ({ usuario, pedido }) => {};
+const cancelarPedido = ({ usuario, pedido }) => {
+  const message = `
+    <h1 style="text-align: center;">Pedido Cancelado</h1>
+    <br />
+    <p>O pedido feito no dia ${moment(pedido.createdAt).format(
+      'DD/MM/YYYY',
+    )} foi cancelado.</p>
+    <br />
+    <a href="${loja}">Acesse a loja para saber mais.</a>
+    <br /><br />
+    <p>Atenciosamente,</p>
+    <p>Equipe - Loja TI</p>
+  `;
+  _send({
+    subject: 'Pedido Cancelado - Loja TI',
+    emails: usuario.email,
+    message,
+  });
+};
 
 // Atualizacao de Pagamento e Entrega
-const atualizarPedido = ({ usuario, pedido, status, data, tipo }) => {};
+const atualizarPedido = ({ usuario, pedido, status, data, tipo }) => {
+  const message = `
+    <h1 style="text-align: center;">Pedido Atualizado</h1>
+    <br />
+    <p>O pedido feito no dia ${moment(pedido.createdAt).format(
+      'DD/MM/YYYY',
+    )} teve uma atualização.</p>
+    <br />
+    <p>Nova Atualização: ${status} - realizado em ${moment(data).format(
+    'DD/MM/YYYY HH:mm',
+  )}</p>
+    <br />
+    <a href="${loja}">Acesse a loja para saber mais.</a>
+    <br /><br />
+    <p>Atenciosamente,</p>
+    <p>Equipe - Loja TI</p>
+  `;
+  _send({
+    subject: 'Pedido Atualizado - Loja TI',
+    emails: usuario.email,
+    message,
+  });
+};
 
 module.exports = {
   enviarNovoPedido,
