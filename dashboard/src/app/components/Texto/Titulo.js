0957608ec42c2478972fd6daf6b5ba16a51bef97
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-02-15 20:33:12 -0300

    Order Details Screen 2

--- dashboard/src/app/components/Texto/Titulo.js
+++ dashboard/src/app/components/Texto/Titulo.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Titulo = ({ tipo, titulo }) => {
   switch (tipo) {
+    case 'h4':
+      return <h4 className="Subtitulo">{titulo}</h4>;
     case 'h2':
-      return <h2 className="Titulo-principal">{titulo}</h2>;
+      return <h2 className="Titulo-secundario">{titulo}</h2>;
     case 'h1':
     default:
       return <h1 className="Titulo-principal">{titulo}</h1>;
--- dashboard/src/app/containers/Pedido/DetalhesDoPedido.js
+++ dashboard/src/app/containers/Pedido/DetalhesDoPedido.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ButtonSimples from '../../components/Button/Simples';
 import Titulo from '../../components/Texto/Titulo';
+import { TextoDados } from '../../components/Texto/Dados';
+import TabelaSimples from '../../components/Tabela/Simples';
 
 class DetalhesDoPedido extends Component {
   renderCabecalho() {
@@ -20,8 +22,83 @@ class DetalhesDoPedido extends Component {
     );
   }
 
+  renderDadosDoCliente() {
+    return (
+      <div>
+        <Titulo tipo="h4" titulo="Dados do Cliente" />
+        <br />
+        <TextoDados chave="Nome" valor="Cliente 1" />
+        <TextoDados chave="CPF" valor="111.222.333-45" />
+        <TextoDados chave="Telefone" valor="12 1234-5678" />
+        <TextoDados chave="Data de Nascimento" valor="10/04/1990" />
+      </div>
+    );
+  }
+
+  renderDadosDeEntrega() {
+    return (
+      <div>
+        <Titulo tipo="h4" titulo="Dados de Entrega" />
+        <br />
+        <TextoDados chave="Endereço" valor="Rua Logo Ali, 123" />
+        <TextoDados chave="Bairro" valor="Centro" />
+        <TextoDados chave="Cidade" valor="São Paulo" />
+        <TextoDados chave="Estado" valor="São Paulo" />
+        <TextoDados chave="CEP" valor="11123-456" />
+      </div>
+    );
+  }
+
+  renderDadosDePagamento() {
+    return (
+      <div>
+        <Titulo tipo="h4" titulo="Dados de Pagamento" />
+        <br />
+        <TextoDados chave="Taxa de Entrega" valor="R$ 15,50 (PAC)" />
+        <TextoDados chave="Valor do Pedido" valor="R$ 32,00" />
+        <TextoDados chave="Valor Total" valor="R$ 47,50" />
+        <TextoDados chave="Forma de Pagamento" valor="Boleto" />
+      </div>
+    );
+  }
+
+  renderDadosDoCarrinho() {
+    const dados = [
+      {
+        'Produto': 'Produto 1',
+        'Preço Und.': 'R$ 12,00',
+        'Quantidade': '1',
+        'Preço Total': 'R$ 12,00',
+      },
+      {
+        'Produto': 'Produto 2',
+        'Preço Und.': 'R$ 10,00',
+        'Quantidade': '2',
+        'Preço Total': 'R$ 20,00',
+      }
+    ]
+    return (
+      <div>
+        <Titulo tipo="h4" titulo="Carrinho" />
+        <br />
+        <TabelaSimples
+          cabecalho={['Produto', 'Preço Und.', 'Quantidade', 'Preço Total']}
+          dados={dados}
+        />
+      </div>
+    );
+  }
+
   render() {
-    return <div className="Detalhes-do-Pedido">{this.renderCabecalho()}</div>;
+    return (
+      <div className="Detalhes-do-Pedido">
+        {this.renderCabecalho()}
+        {this.renderDadosDoCliente()}
+        {this.renderDadosDoCarrinho()}
+        {this.renderDadosDeEntrega()}
+        {this.renderDadosDePagamento()}
+      </div>
+    );
   }
 }
 
