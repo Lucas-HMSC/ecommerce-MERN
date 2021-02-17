c4870bbb1803dae37c4498cc3f18ad82caadf8b3
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-02-17 20:00:55 -0300

    Update index.css

--- dashboard/src/app/containers/Base/BarraTopo.js
+++ dashboard/src/app/containers/Base/BarraTopo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const BarraTopo = () => (
-  <div className="flex horizontal full-width">
+  <div className="Barra-Topo flex horizontal full-width">
     <div className="flex-1 flex flex-start">
       <a href="/">Ver Loja</a>
     </div>
