b22cfb545037a0d74a4bbe370f09a722c2f7b896
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-02-21 18:36:56 -0300

    Settings Page

--- dashboard/src/app/index.js
+++ dashboard/src/app/index.js
@@ -22,6 +22,8 @@ import Produto from './containers/Produto';
 import Avaliacoes from './containers/Avaliacoes';
 import Avaliacao from './containers/Avaliacao';
 
+import Configuracoes from './containers/Configuracoes';
+
 // Containers sem Base
 import Login from './containers/Login';
 import RecuperarSenha from './containers/RecuperarSenha';
@@ -47,6 +49,8 @@ class App extends Component {
 
             <Route path={'/avaliacoes/:id'} component={base(Avaliacoes)} />
             <Route path={'/avaliacao/:id'} component={base(Avaliacao)} />
+
+            <Route path={'/configuracoes'} component={base(Configuracoes)} />
 
             <Route path={'/login'} component={Login} />
             <Route path={'/recuperar-senha'} component={RecuperarSenha} />
