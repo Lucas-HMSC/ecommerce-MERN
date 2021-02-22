42ba78355b828232ae758b3ba58d8af06ef94d20
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-02-22 11:57:43 -0300

    Update Select.js

--- dashboard/src/app/components/Inputs/Select.js
+++ dashboard/src/app/components/Inputs/Select.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default ({ value, name, opcoes, onChange }) => (
-  <div>
+  <div className="flex vertical">
     <select value={value} name={name} onChange={onChange}>
       {opcoes.map((opcao, idx) => (
         <option key={opcao.value} value={opcao.value}>
