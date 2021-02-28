b93625aac8c99085bd9f52fd2b9101ce0bd32074
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-02-28 12:10:24 -0300

    Form's Validation

--- dashboard/src/app/components/Inputs/Simples.js
+++ dashboard/src/app/components/Inputs/Simples.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const InputSimples = ({ type, label, value, onChange }) => (
+const InputSimples = ({ type, label, value, onChange, error }) => (
   <div className="Input-Simples flex vertical">
     {label && <label>{label}</label>}
-    <input type={type} value={value} onChange={onChange} />
+    {error && <small className="small-danger">{error}</small>}
+    <input
+      type={type}
+      value={value}
+      onChange={onChange}
+      className={`${error ? 'input-error' : ''}`}
+    />
   </div>
 );
 
--- dashboard/src/app/containers/Login/index.js
+++ dashboard/src/app/containers/Login/index.js
@@ -17,20 +17,34 @@ class Login extends Component {
     email: '',
     senha: '',
     opcaoLembrar: true,
+    erros: {},
   };
 
-  onChangeInput = (field, ev) => this.setState({ [field]: ev.target.value });
+  onChangeInput = (field, ev) => {
+    this.setState({ [field]: ev.target.value });
+    this.validate();
+  };
   onChangeCheckbox = (field) => this.setState({ [field]: !this.state[field] });
 
   handleLogin() {
     const { email, senha: password, opcaoLembrar } = this.state;
+    if (!this.validate()) return;
     this.props.handleLogin({ email, password, opcaoLembrar }, () => {
       alert('aviso');
     });
   }
 
+  validate() {
+    const { email, senha } = this.state;
+    const erros = {};
+    if (!email) erros.email = 'Preencha aqui com o seu e-mail';
+    if (!senha) erros.senha = 'Preencha aqui com a sua senha';
+    this.setState({ erros });
+    return !(Object.keys(erros).length > 0);
+  }
+
   render() {
-    const { email, senha, opcaoLembrar } = this.state;
+    const { email, senha, opcaoLembrar, erros } = this.state;
     return (
       <div className="Login flex flex-center">
         <div className="Card">
@@ -46,12 +60,14 @@ class Login extends Component {
             label="E-mail"
             value={email}
             type="email"
+            error={erros.email}
             onChange={(ev) => this.onChangeInput('email', ev)}
           />
           <Input
             label="Senha"
             value={senha}
             type="password"
+            error={erros.senha}
             onChange={(ev) => this.onChangeInput('senha', ev)}
           />
 
@@ -67,7 +83,7 @@ class Login extends Component {
               {/*<Link to="/recuperar-senha">
                 <small>Esqueceu sua senha?</small>
               </Link>*/}
-              <a href={`${api}/${versaio}/api/usuarios/recuperar-senha`}>
+              <a href={`${api}/${versao}/api/usuarios/recuperar-senha`}>
                 <small>Esqueceu sua senha?</small>
               </a>
             </div>
