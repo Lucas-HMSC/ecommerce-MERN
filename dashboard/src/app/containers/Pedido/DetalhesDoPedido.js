b1c21e690b916964a0f64e60c46a3dce41a1753c
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-02-28 19:21:41 -0300

    Update DetalhesDoPedido.js
    Placing Dynamic Data

--- dashboard/src/app/containers/Pedido/DetalhesDoPedido.js
+++ dashboard/src/app/containers/Pedido/DetalhesDoPedido.js
@@ -4,12 +4,24 @@ import Titulo from '../../components/Texto/Titulo';
 import { TextoDados } from '../../components/Texto/Dados';
 import TabelaSimples from '../../components/Tabela/Simples';
 
+import { connect } from 'react-redux';
+import { formatMoney } from '../../actions';
+import moment from 'moment';
+import * as actions from '../../actions/pedidos';
+
 class DetalhesDoPedido extends Component {
   renderCabecalho() {
+    if (!this.props.pedido) return null;
+    const { pedido } = this.props.pedido;
     return (
       <div className="flex">
         <div className="flex-1 flex">
-          <Titulo tipo="h2" titulo="Pedido - Cliente 1 - 15/02/2021" />
+          <Titulo
+            tipo="h2"
+            titulo={`Pedido - ${
+              pedido.cliente ? pedido.cliente.nome : ''
+            } - ${moment(pedido.createdAt).format('DD/MM/YYYY')}`}
+          />
         </div>
         <div className="flex-1 flex flex-end">
           <ButtonSimples
@@ -23,60 +35,96 @@ class DetalhesDoPedido extends Component {
   }
 
   renderDadosDoCliente() {
+    if (!this.props.pedido) return null;
+    const { cliente } = this.props.pedido.pedido;
     return (
       <div className="flex-2">
         <Titulo tipo="h4" titulo="Dados do Cliente" />
         <br />
-        <TextoDados chave="Nome" valor="Cliente 1" />
-        <TextoDados chave="CPF" valor="111.222.333-45" />
-        <TextoDados chave="Telefone" valor="12 1234-5678" />
-        <TextoDados chave="Data de Nascimento" valor="10/04/1990" />
+        <TextoDados chave="Nome" valor={cliente ? cliente.nome : ''} />
+        <TextoDados chave="CPF" valor={cliente ? cliente.cpf : ''} />
+        <TextoDados
+          chave="Telefone"
+          valor={cliente ? cliente.telefones[0] : ''}
+        />
+        <TextoDados
+          chave="Data de Nascimento"
+          valor={
+            cliente ? moment(cliente.dataDeNascimento).format('DD/MM/YYYY') : ''
+          }
+        />
       </div>
     );
   }
 
   renderDadosDeEntrega() {
+    if (!this.props.pedido) return null;
+    const { entrega } = this.props.pedido.pedido;
     return (
       <div className="flex-2">
         <Titulo tipo="h4" titulo="Dados de Entrega" />
         <br />
-        <TextoDados chave="Endereço" valor="Rua Logo Ali, 123" />
-        <TextoDados chave="Bairro" valor="Centro" />
-        <TextoDados chave="Cidade" valor="São Paulo" />
-        <TextoDados chave="Estado" valor="São Paulo" />
-        <TextoDados chave="CEP" valor="11123-456" />
+        <TextoDados
+          chave="Endereco"
+          valor={entrega ? entrega.endereco.local : ''}
+        />
+        <TextoDados
+          chave="Numero"
+          valor={entrega ? entrega.endereco.numero : ''}
+        />
+        <TextoDados
+          chave="Bairro"
+          valor={entrega ? entrega.endereco.bairro : ''}
+        />
+        <TextoDados
+          chave="Cidade"
+          valor={entrega ? entrega.endereco.cidade : ''}
+        />
+        <TextoDados
+          chave="Estado"
+          valor={entrega ? entrega.endereco.estado : ''}
+        />
+        <TextoDados chave="CEP" valor={entrega ? entrega.endereco.CEP : ''} />
       </div>
     );
   }
 
   renderDadosDePagamento() {
+    if (!this.props.pedido) return null;
+    const { entrega, pagamento } = this.props.pedido.pedido;
     return (
       <div className="flex-3">
         <Titulo tipo="h4" titulo="Dados de Pagamento" />
         <br />
-        <TextoDados chave="Taxa de Entrega" valor="R$ 15,50 (PAC)" />
-        <TextoDados chave="Valor do Pedido" valor="R$ 32,00" />
-        <TextoDados chave="Valor Total" valor="R$ 47,50" />
-        <TextoDados chave="Forma de Pagamento" valor="Boleto" />
+        <TextoDados
+          chave="Taxa de Entrega"
+          valor={`${formatMoney(entrega.custo)} (${entrega.tipo})`}
+        />
+        <TextoDados
+          chave="Valor do Pedido"
+          valor={`${formatMoney(pagamento.valor - entrega.custo)}`}
+        />
+        <TextoDados
+          chave="Valor Total"
+          valor={`${formatMoney(pagamento.valor)}`}
+        />
+        <TextoDados chave="Forma de Pagamento" valor={pagamento.forma} />
       </div>
     );
   }
 
   renderDadosDoCarrinho() {
-    const dados = [
-      {
-        Produto: 'Produto 1',
-        'Preço Und.': 'R$ 12,00',
-        Quantidade: '1',
-        'Preço Total': 'R$ 12,00',
-      },
-      {
-        Produto: 'Produto 2',
-        'Preço Und.': 'R$ 10,00',
-        Quantidade: '2',
-        'Preço Total': 'R$ 20,00',
-      },
-    ];
+    if (!this.props.pedido) return null;
+    const { carrinho } = this.props.pedido.pedido;
+    const dados = [];
+    carrinho.forEach((item) => {
+      dados.push({
+        Produto: item.produto.titulo + ' - ' + item.variacao.nome,
+        'Preço Und.': formatMoney(item.precoUnitario),
+        Quantidade: item.quantidade,
+        'Preço Total': formatMoney(item.precoUnitario * item.quantidade),
+      });
+    });
     return (
       <div className="flex-3">
         <Titulo tipo="h4" titulo="Carrinho" />
@@ -108,4 +156,9 @@ class DetalhesDoPedido extends Component {
   }
 }
 
-export default DetalhesDoPedido;
+const mapStateToProps = (state) => ({
+  pedido: state.pedido.pedido,
+  usuario: state.auth.usuario,
+});
+
+export default connect(mapStateToProps, actions)(DetalhesDoPedido);
