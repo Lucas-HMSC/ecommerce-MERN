3956cb81dcff9473974b2827e1e91e746a45521d
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-02-28 21:27:04 -0300

    Payment & Delivery Details

--- dashboard/src/app/containers/Pedido/DetalhesDaEntrega.js
+++ dashboard/src/app/containers/Pedido/DetalhesDaEntrega.js
@@ -4,39 +4,80 @@ import Titulo from '../../components/Texto/Titulo';
 import ListaDinamica from '../../components/Listas/ListaDinamicaSimples';
 import InputValor from '../../components/Inputs/InputValor';
 
+import * as actions from '../../actions/pedidos';
+import { connect } from 'react-redux';
+import AlertGeral from '../../components/Alert/Geral';
+
 class DetalhesDaEntrega extends Component {
   state = {
-    status: [
-      'Preparando para Envio',
-      'Entregue a Transportadora',
-      'Em Trânsito',
-    ],
-    codigoDeRastreamento: 'PA123456912BR',
+    aviso: null,
   };
 
+  cleanState() {
+    this.setState({ aviso: null });
+  }
+
   onAddListaDinamica = (texto) => {
-    if (!texto) return false;
-    let { status } = this.state;
-    status.push(texto);
-    this.setState({ status });
+    if (!texto)
+      return this.setState({
+        aviso: {
+          status: false,
+          msg: 'Preencha o campo para enviar um novo status',
+        },
+      });
+    this.setNovoStatus(texto, undefined);
+  };
+
+  setNovoStatus = (status, codigoRastreamento) => {
+    this.cleanState();
+    const { pedido, usuario } = this.props;
+    if (!usuario || !pedido) return false;
+    this.props.setNovoStatusEntrega(
+      { status, codigoRastreamento },
+      pedido.pedido.entrega._id,
+      pedido.pedido._id,
+      usuario.loja,
+      (error) => {
+        if (error) this.setState({ aviso: false, msg: error.message });
+      },
+    );
   };
 
   handleSubmit = (value) => {
-    this.setState({ codigoDeRastreamento: value });
-    alert('SALVO!');
+    if (!value)
+      return this.setState({
+        aviso: {
+          status: false,
+          msg: 'Preencha o código de rastreamento corretamente',
+        },
+      });
+    this.setNovoStatus('Atualização no Cód. de Rastreamente', value);
   };
 
   render() {
-    const { status, codigoDeRastreamento } = this.state;
+    const { pedido } = this.props;
+    const { aviso } = this.state;
+
+    if (!pedido) return <div></div>;
+
+    const status = (pedido.registros || []).reduce(
+      (all, item) =>
+        item.tipo === 'entrega' ? all.concat([item.situacao]) : all,
+      [],
+    );
+
+    const { codigoRastreamento } = pedido.pedido.entrega;
+
     return (
       <div className="Detalhes-de-Entrega">
         <Titulo tipo="h3" titulo="Entrega" />
+        <AlertGeral aviso={aviso} />
         <br />
         <label>Código de Rastreamento</label>
         <InputValor
-          value={codigoDeRastreamento}
+          value={codigoRastreamento}
           handleSubmit={(value) => this.handleSubmit(value)}
-          name={'codigoDeRastreamento'}
+          name={'codigoRastreamento'}
         />
         <br />
         <ListaDinamica dados={status} onAdd={this.onAddListaDinamica} />
@@ -45,4 +86,9 @@ class DetalhesDaEntrega extends Component {
   }
 }
 
-export default DetalhesDaEntrega;
+const mapStateToProps = (state) => ({
+  pedido: state.pedido.pedido,
+  usuario: state.auth.usuario,
+});
+
+export default connect(mapStateToProps, actions)(DetalhesDaEntrega);
