f70ddf1081cbf90d8236a351a9c2a59048c317c7
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-03-04 21:38:49 -0300

    update index.js

--- dashboard/src/app/containers/Cliente/index.js
+++ dashboard/src/app/containers/Cliente/index.js
@@ -3,19 +3,37 @@ import React, { Component } from 'react';
 import DetalhesDoCliente from './detalhesDoCliente';
 import DetalhesDosPedidos from './detalhesDosPedidos';
 
+import { connect } from 'react-redux';
+import { actions } from '../../actions/clientes';
+
 class Cliente extends Component {
+  componentDidMount() {
+    const { usuario } = this.props;
+    const { id } = this.props.match.params;
+    if (!usuario) return null;
+    this.props.getCliente(id, usuario.loja);
+  }
+
+  componentWillUnmount() {
+    this.props.limparCliente();
+  }
+
   render() {
     return (
       <div className="Cliente full-width flex vertical">
         <div className="Card">
-          <DetalhesDoCliente />
+          <DetalhesDoCliente history={this.props.history} />
         </div>
         <div className="Sub-card">
-          <DetalhesDosPedidos />
+          <DetalhesDosPedidos id={this.props.match.params.id} />
         </div>
       </div>
     );
   }
 }
 
-export default Cliente;
+const mapStateToProps = (state) => ({
+  usuario: state.auth.usuario,
+});
+
+export default connect(mapStateToProps, actions)(Cliente);
