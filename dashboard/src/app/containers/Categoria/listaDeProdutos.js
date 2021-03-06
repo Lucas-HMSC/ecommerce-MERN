dc110658686e9abf7cdfecdfede08ccdd3a60021
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-03-06 12:26:10 -0300

    Update listaDeProdutos.js
    Created Products List

--- dashboard/src/app/containers/Categoria/listaDeProdutos.js
+++ dashboard/src/app/containers/Categoria/listaDeProdutos.js
@@ -2,78 +2,72 @@ import React, { Component } from 'react';
 
 import Titulo from '../../components/Texto/Titulo';
 
-import Pesquisa from '../../components/Inputs/Pesquisa';
 import Tabela from '../../components/Tabela/Simples';
 import Paginacao from '../../components/Paginacao/Simples.js';
 
+import { connect } from 'react-redux';
+import * as actions from '../../actions/categorias';
+
 class ListaDeProdutos extends Component {
   state = {
-    pesquisa: '',
     atual: 0,
+    limit: 5,
   };
 
-  onChangePesquisa = (ev) => this.setState({ pesquisa: ev.target.value });
+  getCategoriaProdutos(props) {
+    const { atual, limit } = this.state;
+    const { usuario, categoria } = props;
 
-  changeNumeroAtual = (atual) => this.setState({ atual });
+    if (!usuario || !categoria) return null;
+    this.props.getCategoriaProdutos(categoria._id, atual, limit, usuario.loja);
+  }
+
+  componentDidMount() {
+    this.getCategoriaProdutos(this.props);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (
+      (!prevProps.usuario && this.props.usuario) ||
+      (!prevProps.categoria && this.props.categoria)
+    )
+      this.getCategoriaProdutos(this.props);
+  }
+
+  changeNumeroAtual = (atual) =>
+    this.setState({ atual }, () => this.getCategoriaProdutos(this.props));
 
   render() {
-    const { pesquisa } = this.state;
+    const { categoriaProdutos } = this.props;
 
-    // Dados
-    const dados = [
-      {
-        'Produto': 'Mouse 1',
-        'Estoque': 20,
-        'Disponibilidade': 'sim',
-        'botaoDetalhes': '/produto/9F2J39FKEW91'
-      },
-      {
-        'Produto': 'Mouse 2',
-        'Estoque': 20,
-        'Disponibilidade': 'não',
-        'botaoDetalhes': '/produto/9F2J39FKEW92'
-      },
-      {
-        'Produto': 'Mouse 3',
-        'Estoque': 20,
-        'Disponibilidade': 'não',
-        'botaoDetalhes': '/produto/9F2J39FKEW93'
-      },
-      {
-        'Produto': 'Mouse 4',
-        'Estoque': 20,
-        'Disponibilidade': 'sim',
-        'botaoDetalhes': '/produto/9F2J39FKEW94'
-      },
-      {
-        'Produto': 'Mouse 5',
-        'Estoque': 20,
-        'Disponibilidade': 'sim',
-        'botaoDetalhes': '/produto/9F2J39FKEW95'
-      },
-    ];
+    const dados = [];
+    (categoriaProdutos ? categoriaProdutos.docs : []).forEach((item) => {
+      dados.push({
+        Produto: item.titulo,
+        SKU: item.sku,
+        Disponibilidade: item.disponibilidade ? 'Disponível' : 'Indisponível',
+        botaoDetalhes: `/produto/${item._id}`,
+      });
+    });
 
     return (
       <div className="ListaDeProdutos">
-        <br/>
-        <hr/>
+        <br />
+        <hr />
         <Titulo tipo="h3" titulo="Produtos da Categoria" />
         <br />
-        <Pesquisa
-          valor={pesquisa}
-          placeholder={'Pesquise aqui pelo nome do produto ou descrição...'}
-          onChange={(ev) => this.onChangePesquisa(ev)}
-          onClick={() => alert('Pesquisar')}
-        />
-        <br />
         <Tabela
-          cabecalho={['Produto', 'Estoque', 'Disponibilidade']}
+          cabecalho={['Produto', 'SKU', 'Disponibilidade']}
           dados={dados}
         />
         <Paginacao
           atual={this.state.atual}
-          total={120}
-          limite={20}
+          total={
+            this.props.categoriaProdutos
+              ? this.props.categoriaProdutos.total
+              : 0
+          }
+          limite={this.state.limit}
           onClick={(numeroAtual) => this.changeNumeroAtual(numeroAtual)}
         />
       </div>
@@ -81,4 +75,10 @@ class ListaDeProdutos extends Component {
   }
 }
 
-export default ListaDeProdutos;
+const mapStateToProps = (state) => ({
+  categoriaProdutos: state.categoria.categoriaProdutos,
+  categoria: state.categoria.categoria,
+  usuario: state.auth.usuario,
+});
+
+export default connect(mapStateToProps, actions)(ListaDeProdutos);
