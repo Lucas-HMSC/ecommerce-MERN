e12e336bd9de84f0dcd8b9ae3439600eea1d97dc
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-03-06 15:38:06 -0300

    Created Product List

--- dashboard/src/app/containers/Produtos/index.js
+++ dashboard/src/app/containers/Produtos/index.js
@@ -1,57 +1,67 @@
 import React, { Component } from 'react';
-
 import Titulo from '../../components/Texto/Titulo';
-
 import Pesquisa from '../../components/Inputs/Pesquisa';
 import Tabela from '../../components/Tabela/Simples';
 import Paginacao from '../../components/Paginacao/Simples.js';
+import { Link } from 'react-router-dom';
+
+import { connect } from 'react-redux';
+import * as actions from '../../actions/produtos';
 
 class Produtos extends Component {
   state = {
     pesquisa: '',
     atual: 0,
+    limit: 5,
+    ordem: 'alfabetica_a-z',
   };
+
+  getProdutos(props) {
+    const { atual, limit, pesquisa, ordem } = this.state.atual;
+    const { usuario } = props;
+
+    if (!usuario) return null;
+
+    if (pesquisa)
+      props.getProdutosPesquisa(pesquisa, ordem, atual, limit, usuario.loja);
+    else props.getProdutos(ordem, atual, limit, usuario.loja);
+  }
+
+  componentDidMount() {
+    this.getProdutos(this.props);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!prevProps.usuario && this.props.usuario) this.getProdutos(this.props);
+  }
+
+  handleSubmitPesquisa() {
+    this.setState({ atual: 0 }, () => this.getProdutos(this.props));
+  }
 
   onChangePesquisa = (ev) => this.setState({ pesquisa: ev.target.value });
 
-  changeNumeroAtual = (atual) => this.setState({ atual });
+  changeNumeroAtual = (atual) =>
+    this.setState({ atual }, () => this.getProdutos(this.props));
+
+  changeOrdem = (ev) =>
+    this.setState({ ordem: ev.target.value }, () =>
+      this.getProdutos(this.props),
+    );
 
   render() {
-    const { pesquisa } = this.state;
+    const { pesquisa, ordem } = this.state;
+    const { produtos } = this.props;
 
-    // Dados
-    const dados = [
-      {
-        'Produto': 'Mouse 1',
-        'Categoria': 'acessorios',
-        'Disponível': 'sim',
-        'botaoDetalhes': '/produto/9F2J39FKEW91',
-      },
-      {
-        'Produto': 'Mouse 2',
-        'Categoria': 'acessorios',
-        'Disponível': 'não',
-        'botaoDetalhes': '/produto/9F2J39FKEW92',
-      },
-      {
-        'Produto': 'Mouse 3',
-        'Categoria': 'acessorios',
-        'Disponível': 'não',
-        'botaoDetalhes': '/produto/9F2J39FKEW93',
-      },
-      {
-        'Produto': 'Mouse 4',
-        'Categoria': 'acessorios',
-        'Disponível': 'sim',
-        'botaoDetalhes': '/produto/9F2J39FKEW94',
-      },
-      {
-        'Produto': 'Mouse 5',
-        'Categoria': 'acessorios',
-        'Disponível': 'sim',
-        'botaoDetalhes': '/produto/9F2J39FKEW95',
-      },
-    ];
+    const dados = [];
+    (produtos ? produtos.docs : []).forEach((item) => {
+      dados.push({
+        Produto: item.titulo,
+        Categoria: item.categoria ? item.categoria.nome : '',
+        Disponível: item.disponibilidade ? 'sim' : 'não',
+        botaoDetalhes: `/produto/${item._id}`,
+      });
+    });
 
     return (
       <div className="Produtos full-width">
@@ -62,24 +72,26 @@ class Produtos extends Component {
             <div className="flex-3">
               <Pesquisa
                 valor={pesquisa}
-                placeholder={'Pesquise aqui pelo nome do produto, descrição ou categoria...'}
+                placeholder={
+                  'Pesquise aqui pelo nome do produto, descrição ou categoria...'
+                }
                 onChange={(ev) => this.onChangePesquisa(ev)}
-                onClick={() => alert('Pesquisar')}
+                onClick={() => this.handleSubmitPesquisa()}
               />
             </div>
             <div className="flex-1 flex vertical">
               <label>
                 <small>Ordenar por</small>
               </label>
-              <select defaultValue="">
+              <select value={ordem} onChange={this.changeOrdem}>
                 <option>Aleatório</option>
-                <option value={'oaA-Z'}>Alfabética A-Z</option>
-                <option value={'oaZ-A'}>Alfabética Z-A</option>
-                <option value={'op-menor'}>Menor Preço</option>
-                <option value={'op-maior'}>Maior Preço</option>
+                <option value={'alfabetica_a-z'}>Alfabética A-Z</option>
+                <option value={'alfabetica_z-a'}>Alfabética Z-A</option>
+                <option value={'preco-crescente'}>Menor Preço</option>
+                <option value={'preco-decrescente'}>Maior Preço</option>
               </select>
             </div>
-            <div className='flex-1'></div>
+            <div className="flex-1"></div>
           </div>
           <br />
           <Tabela
@@ -88,8 +100,8 @@ class Produtos extends Component {
           />
           <Paginacao
             atual={this.state.atual}
-            total={120}
-            limite={20}
+            total={this.props.produtos ? this.props.produtos.total : 0}
+            limite={this.state.limit}
             onClick={(numeroAtual) => this.changeNumeroAtual(numeroAtual)}
           />
         </div>
@@ -98,4 +110,9 @@ class Produtos extends Component {
   }
 }
 
-export default Produtos;
+const mapStateToProps = (state) => ({
+  produtos: state.produto.produtos,
+  usuario: state.auth.usuario,
+});
+
+export default connect(mapStateToProps, actions)(Produtos);
