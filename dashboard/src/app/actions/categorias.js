a918be91c1e57b0ea2a5d02913a09b1d1da35be7
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-03-06 11:31:36 -0300

    Categories - Actions & Reducer

--- dashboard/src/app/actions/categorias.js
+++ dashboard/src/app/actions/categorias.js
@@ -2,7 +2,13 @@ import { getHeaders } from './localStorage';
 import axios from 'axios';
 import { api, versao } from '../config';
 import errorHandling from './errorHandling';
-import { GET_CATEGORIAS, GET_CATEGORIA } from './types';
+import {
+  GET_CATEGORIAS,
+  GET_CATEGORIA,
+  LIMPAR_CATEGORIA,
+  REMOVE_CATEGORIA,
+  GET_CATEGORIA_PRODUTOS,
+} from './types';
 
 export const getCategorias = (loja) => {
   return function (dispatch) {
@@ -32,6 +38,81 @@ export const salvarCategoria = (categoria, loja, cb) => {
       .then((response) => {
         dispatch({
           type: GET_CATEGORIA,
+          payload: response.data,
+        });
+        cb(null);
+      })
+      .catch((e) => cb(errorHandling(e)));
+  };
+};
+
+export const getCategoria = (id, loja) => {
+  return function (dispatch) {
+    axios
+      .get(`${api}/${versao}/api/categorias/${id}?loja=${loja}`, getHeaders())
+      .then((response) =>
+        dispatch({
+          type: GET_CATEGORIA,
+          payload: response.data,
+        }),
+      )
+      .catch(errorHandling);
+  };
+};
+
+export const limparCategoria = () => ({ type: LIMPAR_CATEGORIA });
+
+export const getCategoriaProdutos = (id, atual, limit, loja) => {
+  return function (dispatch) {
+    axios
+      .get(
+        `${api}/${versao}/api/categorias/${id}/produtos?loja=${loja}&offset=${atual}&limit=${limit}`,
+        getHeaders(),
+      )
+      .then((response) =>
+        dispatch({
+          type: GET_CATEGORIA_PRODUTOS,
+          payload: response.data,
+        }),
+      )
+      .catch(errorHandling);
+  };
+};
+
+export const updateCategoria = (categoria, id, loja, cb) => {
+  return function (dispatch) {
+    axios
+      .put(
+        `${api}/${versao}/api/categorias/${id}?loja=${loja}`,
+        {
+          nome: categoria.nome,
+          codigo: categoria.codigo,
+          disponibilidade:
+            categoria.disponibilidade === 'disponivel' ? 'true' : 'false',
+        },
+        getHeaders(),
+      )
+      .then((response) => {
+        dispatch({
+          type: GET_CATEGORIA,
+          payload: response.data,
+        });
+        cb(null);
+      })
+      .catch((e) => cb(errorHandling(e)));
+  };
+};
+
+export const removerCategoria = (id, loja, cb) => {
+  return function (dispatch) {
+    axios
+      .delete(
+        `${api}/${versao}/api/categorias/${id}?loja=${loja}`,
+        getHeaders(),
+      )
+      .then((response) => {
+        dispatch({
+          type: REMOVE_CATEGORIA,
           payload: response.data,
         });
         cb(null);
--- dashboard/src/app/reducers/categoria_reducer.js
+++ dashboard/src/app/reducers/categoria_reducer.js
@@ -1,4 +1,10 @@
-import { GET_CATEGORIAS, GET_CATEGORIA } from '../actions/types';
+import {
+  GET_CATEGORIAS,
+  GET_CATEGORIA,
+  GET_CATEGORIA_PRODUTOS,
+  LIMPAR_CATEGORIA,
+  REMOVE_CATEGORIA,
+} from '../actions/types';
 
 export default (state = {}, action) => {
   switch (action.type) {
@@ -11,6 +17,17 @@ export default (state = {}, action) => {
       return {
         ...state,
         categoria: action.payload.categoria,
+      };
+    case LIMPAR_CATEGORIA:
+    case REMOVE_CATEGORIA:
+      return {
+        ...state,
+        categoria: null,
+      };
+    case GET_CATEGORIA_PRODUTOS:
+      return {
+        ...state,
+        categoriaProdutos: action.payload.produtos,
       };
     default:
       return state;
