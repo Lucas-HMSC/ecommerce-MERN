756f340aecdc8cbd88d090153ac64b0c6b02a4ae
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-03-06 11:37:09 -0300

    update index.js
    Initial Config

--- dashboard/src/app/containers/Categoria/index.js
+++ dashboard/src/app/containers/Categoria/index.js
@@ -3,13 +3,27 @@ import React, { Component } from 'react';
 import DetalhesCategoria from './detalhesCategoria';
 import ListaDeProdutos from './listaDeProdutos';
 
+import { connect } from 'react-redux';
+import * as actions from '../../actions/categorias';
+
 class Categoria extends Component {
+  componentDidMount() {
+    const { usuario } = this.props;
+    const { id } = this.props.match.params;
+    if (!usuario || !id) return null;
+    this.props.getCategoria(id, usuario.loja);
+  }
+
+  componentWillUnmount() {
+    this.props.limparCategoria();
+  }
+
   render() {
     return (
       <div className="Categoria full-width">
         <div className="Card">
           <div>
-            <DetalhesCategoria />
+            <DetalhesCategoria history={this.props.history} />
           </div>
           <div>
             <ListaDeProdutos />
@@ -20,4 +34,8 @@ class Categoria extends Component {
   }
 }
 
-export default Categoria;
+const mapStateToProps = (state) => ({
+  usuario: state.auth.usuario,
+});
+
+export default connect(mapStateToProps, actions)(Categoria);
