cae29f233f2a94075e0997973a409590a11e81c4
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-03-09 21:15:02 -0300

    Create cookie.js
    Created Helper for Cookies

---
+++ lojavirtual/utils/cookie.js
@@ -0,0 +1,29 @@
+import cookie, { get } from 'js-cookie';
+
+export const setCookie = (key, value) => {
+  if (process.browser)
+    cookie.set(key, value, {
+      expires: 1,
+      path: '/',
+    });
+};
+
+export const removeCookie = (key) => {
+  if (process.browser) cookie.remove(key, { expire: 1 });
+};
+
+export const getCookie = (key, rew) => {
+  return process.browser
+    ? getCookieFromBrowser(key)
+    : getCookieFromServer(key, req);
+};
+
+const getCookieFromBrowser = (key) => cookie.get(key);
+
+const getCookieFromServer = (key, req) => {
+  if (!req.headers.cookie) return undefined;
+  const _cookie = req.headers.cookie
+    .split(';')
+    .find((c) => c.trim().startsWith(`${key}=`));
+  return _cookie ? _cookie.split('=')[1] : undefined;
+};
