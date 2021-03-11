e8694a562f0201b4f5299c73ff5ccb5e74e251ec
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-03-10 21:17:59 -0300

    Create Head.js

---
+++ lojavirtual/components/Head.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import NextHead from 'next/head';
+import { string } from 'prop-types';
+
+const defaultDescricao = '';
+const defaultOGURL = '';
+const defaultOGImage = '/static/logo.png';
+
+const Head = (props) => (
+  <NextHead>
+    <meta charSet="UTF-8" />
+    <title>{props.title || ''}</title>
+    <meta name="description" content={props.description || defaultDescricao} />
+    <meta name="viewport" content="width=device-width, initak-scale=1" />
+    <link rel="icon" href="/static/favicon.ico" />
+    <meta property="og:url" content={props.url || defaultOGURL} />
+    <meta property="og:title" content={props.title || ''} />
+    <meta
+      property="og:description"
+      content={props.description || defaultDescricao}
+    />
+    <meta property="twitter:site" content={props.url || defaultOGURL} />
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:image" content={props.ogImage || defaultOGImage} />
+    <meta property="og:image" content={props.ogImage || defaultOGImage} />
+    <meta property="og:image:width" content="600" />
+    <meta property="og:image:height" content="600" />
+  </NextHead>
+);
+
+Head.propTypes = {
+  title: string,
+  description: string,
+  url: string,
+  ogImage: string,
+};
+
+export default Head;
