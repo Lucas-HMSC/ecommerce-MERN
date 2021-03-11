fae13f796461c956af9d6437b14cf3bcd0549aa6
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-03-10 21:43:55 -0300

    Basic Store Configuration

---
+++ lojavirtual/components/Style.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => (
+  <style jsx global>{`
+    body {
+      margin: 0;
+      padding: 0;
+    }
+    .flex {
+      display: flex;
+    }
+    .flex-center {
+      justify-content: center;
+      align-items: center;
+    }
+  `}</style>
+);
--- lojavirtual/pages/index.js
+++ lojavirtual/pages/index.js
@@ -1,11 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
+import Layout from '../components/Layout';
+
+import Cabecalho from '../containers/Cabecalho';
+import Banners from '../containers/Banners';
+import Beneficios from '../containers/Beneficios';
+import ProdutosPaginaInicial from '../containers/Lista/ProdutosPaginaInicial';
+import Rodape from '../containers/Rodape';
 
 export default class Index extends Component {
-  render(){
+  render() {
     return (
-      <div>
-        <h2>Loja TI</h2>
-      </div>
-    )
+      <Layout title="Loja TI - Melhores produtos de informática">
+        <Cabecalho />
+        <Banners />
+        <Beneficios />
+        <ProdutosPaginaInicial />
+        <Rodape />
+      </Layout>
+    );
   }
 }
