0ac7bd9cc0cb34f1413b8f2946a9f3cf652bf4f1
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-03-11 21:10:33 -0300

    Products - List & Card 1/2

---
+++ lojavirtual/components/Listas/Produtos.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+import Produto from '../Item/Produto';
+
+class Produtos extends Component {
+  componentDidMount() {
+    let highest = 0;
+    document.querySelectorAll('.produto-title').forEach(function (item) {
+      if (highest < item.clientHeight) highest = item.clientHeight;
+    });
+    document.querySelectorAll('.produto-title').forEach(function (item) {
+      item.style.height = highest + 10 + 'px';
+    });
+  }
+
+  render() {
+    const { produtos, itensPorLinha } = this.props;
+    return (
+      <div className="Produtos flex wrap">
+        {produtos.map((item) => (
+          <Produto item={item} key={item.id} porLinha={itensPorLinha} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Produtos;
+s;
