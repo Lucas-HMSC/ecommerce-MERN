735287f0c5dc16b33ddf78042a2b10e4aa4c995b
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-03-13 12:26:17 -0300

    Search Page & Components

---
+++ lojavirtual/containers/Lista/ProdutosPesquisa/index.js
@@ -0,0 +1,146 @@
+import React, { Component } from 'react';
+
+import Produtos from '../../../components/Listas/Produtos';
+import Paginacao from '../../../components/Paginacao';
+
+const PRODUTOS = [
+  {
+    id: 1,
+    fotos: ['/static/img/mouse-1.png'],
+    titulo: 'Mouse Gamer 1',
+    preco: 35,
+    promocao: 25,
+  },
+  {
+    id: 2,
+    fotos: ['/static/img/mouse-4.png'],
+    titulo: 'Mouse Gamer 2',
+    preco: 55,
+    promocao: 45,
+  },
+  {
+    id: 3,
+    fotos: ['/static/img/mouse-5.png'],
+    titulo: 'Mouse Gamer 3',
+    preco: 105,
+    promocao: 95,
+  },
+  {
+    id: 4,
+    fotos: ['/static/img/mouse-2.png'],
+    titulo: 'Mouse Gamer 4',
+    preco: 160,
+    promocao: 150,
+  },
+  {
+    id: 5,
+    fotos: ['/static/img/mouse-1.png'],
+    titulo: 'Mouse Gamer 1',
+    preco: 35,
+    promocao: 25,
+  },
+  {
+    id: 6,
+    fotos: ['/static/img/mouse-4.png'],
+    titulo: 'Mouse Gamer 2',
+    preco: 55,
+    promocao: 45,
+  },
+  {
+    id: 7,
+    fotos: ['/static/img/mouse-5.png'],
+    titulo: 'Mouse Gamer 3',
+    preco: 105,
+    promocao: 95,
+  },
+  {
+    id: 8,
+    fotos: ['/static/img/mouse-2.png'],
+    titulo: 'Mouse Gamer 4',
+    preco: 160,
+    promocao: 150,
+  },
+  {
+    id: 9,
+    fotos: ['/static/img/mouse-1.png'],
+    titulo: 'Mouse Gamer 1',
+    preco: 35,
+    promocao: 25,
+  },
+  {
+    id: 10,
+    fotos: ['/static/img/mouse-4.png'],
+    titulo: 'Mouse Gamer 2',
+    preco: 55,
+    promocao: 45,
+  },
+  {
+    id: 11,
+    fotos: ['/static/img/mouse-5.png'],
+    titulo: 'Mouse Gamer 3',
+    preco: 105,
+    promocao: 95,
+  },
+  {
+    id: 12,
+    fotos: ['/static/img/mouse-2.png'],
+    titulo: 'Mouse Gamer 4',
+    preco: 160,
+    promocao: 150,
+  },
+  {
+    id: 13,
+    fotos: ['/static/img/mouse-1.png'],
+    titulo: 'Mouse Gamer 1',
+    preco: 35,
+    promocao: 25,
+  },
+  {
+    id: 14,
+    fotos: ['/static/img/mouse-4.png'],
+    titulo: 'Mouse Gamer 2',
+    preco: 55,
+    promocao: 45,
+  },
+  {
+    id: 15,
+    fotos: ['/static/img/mouse-5.png'],
+    titulo: 'Mouse Gamer 3',
+    preco: 105,
+    promocao: 95,
+  },
+  {
+    id: 16,
+    fotos: ['/static/img/mouse-2.png'],
+    titulo: 'Mouse Gamer 4',
+    preco: 160,
+    promocao: 150,
+  },
+];
+
+class ProdutosPesquisa extends Component {
+  state = {
+    atual: 0,
+  };
+  render() {
+    return (
+      <div className="container Categoria-Produtos">
+        <br />
+        <br />
+        <div className="flex flex-center">
+          <h1>Resultados para: Mouse</h1>
+        </div>
+        <br />
+        <Produtos produtos={PRODUTOS} itensPorLinha={4} />
+        <Paginacao
+          atual={this.state.atual || 0}
+          total={PRODUTOS.length * 4}
+          limite={PRODUTOS.length}
+          onClick={(numeroAtual) => this.setState({ atual: numeroAtual })}
+        />
+      </div>
+    );
+  }
+}
+
+export default ProdutosPesquisa;
--- lojavirtual/pages/pesquisa.js
+++ lojavirtual/pages/pesquisa.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
+import Layout from '../components/Layout';
+import Cabecalho from '../containers/Cabecalho';
+import ProdutosPesquisa from '../containers/Lista/ProdutosPesquisa';
+import Rodape from '../containers/Rodape';
+
 export default class Pesquisa extends Component {
   render() {
     return (
-      <div>
-        <h2>Loja TI - Pesquisa</h2>
-      </div>
+      <Layout title="Resultados para Mouse | Loja TI - Melhores produtos de tecnologia">
+        <Cabecalho />
+        <ProdutosPesquisa />
+        <Rodape />
+      </Layout>
     );
   }
 }
