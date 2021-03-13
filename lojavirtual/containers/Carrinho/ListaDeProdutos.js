44a770cd6595bee80ff8db099438ad5677cf02dd
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-03-13 20:08:05 -0300

    Cart - Style 1/2

--- lojavirtual/containers/Carrinho/ListaDeProdutos.js
+++ lojavirtual/containers/Carrinho/ListaDeProdutos.js
@@ -23,15 +23,15 @@ class ListaDeProdutos extends Component {
   renderCabecalhoCarrinho() {
     return (
       <div className="no-mb-flex flex">
-        <div className="flex-6"></div>
+        <div className="flex-4"></div>
         <div className="headline flex-1 flex flex-center">
-          <h3>Quantidade</h3>
+          <h3 className="text-center">Quantidade</h3>
         </div>
         <div className="headline flex-1 flex flex-center">
-          <h3>Preço Unitário</h3>
+          <h3 className="text-center">Preço Unitário</h3>
         </div>
         <div className="headline flex-1 flex flex-center">
-          <h3>Preço Total</h3>
+          <h3 className="text-center">Preço Total</h3>
         </div>
         <div className="flex-1"></div>
       </div>
@@ -43,13 +43,13 @@ class ListaDeProdutos extends Component {
     const nome = item.titulo;
     const { quantidade, precoUnitario } = item;
     return (
-      <div key={item.id} className="flex">
-        <div className="flex-6 flex">
+      <div key={item.id} className="carrinho-item flex">
+        <div className="flex-4 flex">
           <div className="produto-image flex-2 flex flex-center">
             <img src={foto} alt={nome} width="100px" />
           </div>
-          <div className="produto-titulo flex-4">
-            <h3>{nome}</h3>
+          <div className="produto-titulo flex-4 flex flex-start">
+            <h3 className="text-center">{nome}</h3>
           </div>
         </div>
         <div className="flex-1 flex flex-center">
