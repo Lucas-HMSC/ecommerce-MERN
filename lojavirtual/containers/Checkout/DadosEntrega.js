fb31f99c394a32891e8ccca9b5763347ff468410
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-03-14 10:27:59 -0300

    Checkout - Delivery Data 2/2

--- lojavirtual/containers/Checkout/DadosEntrega.js
+++ lojavirtual/containers/Checkout/DadosEntrega.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import FormSimples from '../../components/Inputs/FormSimples';
 
+import { ESTADOS } from '../../utils';
+
 export default class DadosClienteContainer extends Component {
   state = {
     dadosEntregaIgualDadosCobranca: true,
@@ -22,6 +24,13 @@ export default class DadosClienteContainer extends Component {
     },
   };
 
+  onChange = (field, e) => this.setState({ [field]: e.target.value });
+  onChangeCobranca = (field, e) => {
+    const { state } = this;
+    state.dadosCobranca[field] = e.target.value;
+    this.setState(state);
+  };
+
   renderDadosDeEntrega() {
     const {
       dadosEntregaIgualDadosCobranca,
@@ -35,6 +44,9 @@ export default class DadosClienteContainer extends Component {
     } = this.state;
     return (
       <div className="flex-1 flex horizontal">
+        <div>
+          <h2>DADOS DE ENTREGA</h2>
+        </div>
         <div className="flex-1">
           <FormSimples
             value={CEP}
@@ -56,25 +68,172 @@ export default class DadosClienteContainer extends Component {
           </div>
           <div className="flex-1">
             <FormSimples
-              value={CEP}
-              name="CEP"
-              placeholder="12345-789"
-              label="CEP"
-              onChange={(e) => this.onChange('CEP', e)}
+              value={numero}
+              name="numero"
+              placeholder="9999"
+              label="Número"
+              onChange={(e) => this.onChange('numero', e)}
             />
+          </div>
+        </div>
+        <div className="flex-1 flex horizontal">
+          <div className="flex-1">
+            <FormSimples
+              value={bairro}
+              name="bairro"
+              placeholder="Bairro"
+              label="Bairro"
+              onChange={(e) => this.onChange('bairro', e)}
+            />
+          </div>
+          <div className="flex-1">
+            <FormSimples
+              value={complemento}
+              name="complemento"
+              placeholder="complemento"
+              label="Complemento"
+              onChange={(e) => this.onChange('complemento', e)}
+            />
+          </div>
+        </div>
+        <div className="flex-1 flex horizontal">
+          <div className="flex-1">
+            <FormSimples
+              value={cidade}
+              name="cidade"
+              placeholder="Cidade"
+              label="Cidade"
+              onChange={(e) => this.onChange('cidade', e)}
+            />
+          </div>
+          <div className="flex-1">
+            <select value={estado} onChange={(e) => this.onChange('estado', e)}>
+              <option>Selecione...</option>
+              {Object.keys(ESTADOS).map((abbr) => (
+                <option key={abbr} value={abbr}>
+                  {ESTADOS[abbr]}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <br />
+        <div>
+          <input
+            checked={dadosEntregaIgualDadosCobranca}
+            type="checkbox"
+            onChange={() =>
+              this.setState({
+                dadosEntregaIgualDadosCobranca: !dadosEntregaIgualDadosCobranca,
+              })
+            }
+          />
+          <label>
+            &nbsp;Os dados de entrega são iguais aos dados de cobrança
+          </label>
+        </div>
+      </div>
+    );
+  }
+
+  renderDadosDeCobranca() {
+    const {
+      local,
+      numero,
+      bairro,
+      complemento,
+      cidade,
+      estado,
+      CEP,
+    } = this.state.dadosCobranca;
+    return (
+      <div className="flex-1 flex horizontal">
+        <div>
+          <h2>DADOS DE COBRANÇA</h2>
+        </div>
+        <div className="flex-1">
+          <FormSimples
+            value={CEP}
+            name="CEP"
+            placeholder="12345-789"
+            label="CEP"
+            onChange={(e) => this.onChangeCobranca('CEP', e)}
+          />
+        </div>
+        <div className="flex-1 flex horizontal">
+          <div className="flex-1">
+            <FormSimples
+              value={local}
+              name="local"
+              placeholder="Rua, Avenida, ..."
+              label="Endereço (Rua, Avenida, ...)"
+              onChange={(e) => this.onChangeCobranca('local', e)}
+            />
+          </div>
+          <div className="flex-1">
+            <FormSimples
+              value={numero}
+              name="numero"
+              placeholder="9999"
+              label="Número"
+              onChange={(e) => this.onChangeCobranca('numero', e)}
+            />
+          </div>
+        </div>
+        <div className="flex-1 flex horizontal">
+          <div className="flex-1">
+            <FormSimples
+              value={bairro}
+              name="bairro"
+              placeholder="Bairro"
+              label="Bairro"
+              onChange={(e) => this.onChangeCobranca('bairro', e)}
+            />
+          </div>
+          <div className="flex-1">
+            <FormSimples
+              value={complemento}
+              name="complemento"
+              placeholder="complemento"
+              label="Complemento"
+              onChange={(e) => this.onChangeCobranca('complemento', e)}
+            />
+          </div>
+        </div>
+        <div className="flex-1 flex horizontal">
+          <div className="flex-1">
+            <FormSimples
+              value={cidade}
+              name="cidade"
+              placeholder="Cidade"
+              label="Cidade"
+              onChange={(e) => this.onChangeCobranca('cidade', e)}
+            />
+          </div>
+          <div className="flex-1">
+            <select
+              value={estado}
+              onChange={(e) => this.onChangeCobranca('estado', e)}
+            >
+              <option>Selecione...</option>
+              {Object.keys(ESTADOS).map((abbr) => (
+                <option key={abbr} value={abbr}>
+                  {ESTADOS[abbr]}
+                </option>
+              ))}
+            </select>
           </div>
         </div>
       </div>
     );
   }
+
   render() {
+    const { dadosEntregaIgualDadosCobranca } = this.state;
     return (
       <div className="flex-1">
-        <div>
-          <h2>DADOS DE ENTREGA</h2>
-        </div>
         {this.renderDadosDeEntrega()}
-        {this.renderDadosDeCobranca()}
+        {!dadosEntregaIgualDadosCobranca && this.renderDadosDeCobranca()}
       </div>
     );
   }
--- lojavirtual/utils/index.js
+++ lojavirtual/utils/index.js
@@ -1,2 +1,32 @@
 export const formatMoney = (value =
   'R$ ' + (value || 0).toFized(2).replace('.', ','));
+
+export const ESTADOS = {
+  Acre: 'AC',
+  Alagoas: 'AL',
+  Amapá: 'AP',
+  Amazonas: 'AM',
+  Bahia: 'BA',
+  Ceará: 'CE',
+  'Distrito Federal': 'DF',
+  'Espírito Santo': 'ES',
+  Goiás: 'GO',
+  Maranhão: 'MA',
+  'Mato Grosso': 'MT',
+  'Mato Grosso do Sul': 'MS',
+  'Minas Gerais': 'MG',
+  Pará: 'PA',
+  Paraíba: 'PB',
+  Paraná: 'PR',
+  Pernambuco: 'PE',
+  Piauí: 'PI',
+  'Rio de Janeiro': 'RJ',
+  'Rio Grande do Norte': 'RN',
+  'Rio Grande do Sul': 'RS',
+  Rondônia: 'RO',
+  Roraima: 'RR',
+  'Santa Catarina': 'SC',
+  'São Paulo': 'SP',
+  Sergipe: 'SE',
+  Tocantins: 'TO',
+};
