237d24ace72ed8de6eab054cbc66a35bc982e557
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-03-13 21:41:23 -0300

    Checkout - Customer Data 3/3

--- lojavirtual/containers/Cliente/DadosCliente.js
+++ lojavirtual/containers/Cliente/DadosCliente.js
@@ -1,7 +1,35 @@
 import React, { Component } from 'react';
+import FormSimples from '../../components/Inputs/FormSimples';
 
 export default class DadosClienteContainer extends Component {
+  renderDadosRegistro() {
+    return (
+      <div>
+        <FormSimples
+          value={email}
+          name="email"
+          placeholder="E-mail"
+          label="E-mail"
+          onChange={(e) => this.onChange('email', e)}
+        />
+        <br />
+        <FormSimples
+          value={senha}
+          name="senha"
+          placeholder="Senha"
+          label="Senha"
+          onChange={(e) => this.onChange('senha', e)}
+        />
+      </div>
+    );
+  }
+  renderDadosUsuario() {}
   render() {
-    return <div></div>;
+    return (
+      <div className="flex-1">
+        {this.renderDadosRegistro()}
+        {this.renderDadosUsuario()}
+      </div>
+    );
   }
 }
