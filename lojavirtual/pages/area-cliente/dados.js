b3bbebfaa1734953589a2569e4f55e13628525ff
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-03-14 19:42:26 -0300

    Customer Data - Components & Style 1/3

--- lojavirtual/pages/area-cliente/dados.js
+++ lojavirtual/pages/area-cliente/dados.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
+import Layout from '../../components/Layout';
+import Cabecalho from '../../containers/Cabecalho';
+import DadosContainer from '../../containers/AreaDoCliente/Dados';
+import Rodape from '../../containers/Rodape';
+
 export default class DadosDoCliente extends Component {
   render() {
     return (
-      <div>
-        <h2>Loja TI - Dados Do Cliente</h2>
-      </div>
+      <Layout title="Meus Dados | Loja TI - Melhores produtos de tecnologia">
+        <Cabecalho />
+        <DadosContainer />
+        <Rodape />
+      </Layout>
     );
   }
 }
