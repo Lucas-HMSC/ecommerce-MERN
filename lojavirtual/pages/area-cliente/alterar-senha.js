cf4bcb984b08f904a6f56101b1654fa275e4f9dc
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-03-14 20:05:27 -0300

    Customer Data - Components & Style 2/3

--- lojavirtual/pages/area-cliente/alterar-senha.js
+++ lojavirtual/pages/area-cliente/alterar-senha.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
+import Layout from '../../components/Layout';
+import Cabecalho from '../../containers/Cabecalho';
+import AlterarSenhaContainer from '../../containers/AreaDoCliente/AlterarSenha';
+import Rodape from '../../containers/Rodape';
+
 export default class AlterarSenha extends Component {
   render() {
     return (
-      <div>
-        <h2>Loja TI - Alterar Senha</h2>
-      </div>
+      <Layout title="Alterar Senha | Loja TI - Melhores produtos de tecnologia">
+        <Cabecalho />
+        <AlterarSenhaContainer />
+        <Rodape />
+      </Layout>
     );
   }
 }
