fefc7a350437ee53e0b0003edd4d58cc55824c5a
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-03-19 10:28:56 -0300

    Update ListaDeProdutos.js

--- lojavirtual/containers/Carrinho/ListaDeProdutos.js
+++ lojavirtual/containers/Carrinho/ListaDeProdutos.js
@@ -1,23 +1,9 @@
 import React, { Component } from 'react';
-
+import { baseImg } from '../../config';
 import { formatMoney } from '../../utils';
-
-const PRODUTOS = [
-  {
-    id: 192391231,
-    fotos: ['/static/img/mouse-1.png'],
-    titulo: 'Mouse Gamer 1 - M',
-    precoUnitario: 25,
-    quantidade: 1,
-  },
-  {
-    id: 192391234,
-    fotos: ['/static/img/mouse-4.png'],
-    titulo: 'Mouse Gamer 2 - G',
-    precoUnitario: 55,
-    quantidade: 1,
-  },
-];
+import { connect } from 'react-redux';
+import actions from '../../redux/actions';
+import { addCart } from '../../utils/cart';
 
 class ListaDeProdutos extends Component {
   renderCabecalhoCarrinho(semAlteracoes) {
@@ -38,15 +24,51 @@ class ListaDeProdutos extends Component {
     );
   }
 
-  renderProduto(item, semAlteracoes) {
-    const foto = item.fotos[0];
-    const nome = item.titulo;
+  changeQuantidade(e, quantidade, item, index) {
+    if (Number(e.target.value) < 1) return;
+    let novaQuantidade = Number(e.target.value);
+    let change = novaQuantidade - quantidade;
+    if (novaQuantidade >= item.variacao.quantidade)
+      return alert('Não temos essa quantidade em estoque.');
+    addCart(
+      {
+        produto: item.produto._id,
+        variacao: item.variacao._id,
+        quantidade: change,
+        precoUnitario: item.precoUnitario,
+      },
+      false,
+    );
+    this.props.updateQuantidade(change, index);
+  }
+
+  removerProdutoCarrinho(index) {
+    if (window.confirm('Você realmente deseja remover este produto?')) {
+      this.props.removerProduto(index);
+    }
+  }
+
+  renderProduto(item, semAlteracoes, index) {
+    if (
+      !item.variacao ||
+      !item.variacao._id ||
+      !item.produto ||
+      !item.produto ||
+      !item.produto._id
+    )
+      return null;
+
+    const foto =
+      item.variacao.fotos && item.variacao.fotos.length > 0
+        ? item.variacao.fotos[0]
+        : item.produto.fotos[0];
+    const nome = item.produto.titulo + ' - ' + item.variacao.nome;
     const { quantidade, precoUnitario } = item;
     return (
       <div key={item.id} className="carrinho-item flex">
         <div className="flex-4 flex">
           <div className="produto-image flex-2 flex flex-center">
-            <img src={foto} alt={nome} width="100px" />
+            <img src={baseImg + foto} alt={nome} width="100px" />
           </div>
           <div className="produto-titulo flex-4 flex flex-start">
             <h3 className="text-center">{nome}</h3>
@@ -58,8 +80,11 @@ class ListaDeProdutos extends Component {
           ) : (
             <input
               type="number"
-              defaultValue={quantidade}
+              value={quantidade}
               className="produto-quantidade"
+              onChange={(e) =>
+                this.changeQuantidade(e, quantidade, item, index)
+              }
             />
           )}
         </div>
@@ -70,7 +95,10 @@ class ListaDeProdutos extends Component {
           <span>{formatMoney(precoUnitario * quantidade)}</span>
         </div>
         {!semAlteracoes && (
-          <div className="flex-1 flex flex-center">
+          <div
+            className="flex-1 flex flex-center"
+            onClick={() => this.removerProdutoCarrinho(index)}
+          >
             <span className="btn-remover">Remover</span>
           </div>
         )}
@@ -79,18 +107,24 @@ class ListaDeProdutos extends Component {
   }
 
   renderProdutos(semAlteracoes) {
-    return PRODUTOS.map((item) => this.renderProduto(item, semAlteracoes));
+    return this.props.carrinho.map((item, index) =>
+      this.renderProduto(item, semAlteracoes, index),
+    );
   }
 
   render() {
-    const { semAlteracoes } = this.props;
+    const { semAlteracoes, carrinho } = this.props;
     return (
       <div className="Lista-De-Produtos flex vertical">
         {This.renderCabecalhoCarrinho(semAlteracoes)}
-        {this.renderProdutos(semAlteracoes)}
+        {carrinho && this.renderProdutos(semAlteracoes)}
       </div>
     );
   }
 }
 
-export default ListaDeProdutos;
+const mapStateToProps = (state) => ({
+  carrinho: state.carrinho.carrinho,
+});
+
+export default connect(mapStateToProps, actions)(ListaDeProdutos);
