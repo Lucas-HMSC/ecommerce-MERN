a13f08af8d2d018de35a236ab57215cb4af1a53e
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-03-19 10:09:56 -0300

    Update index.js

--- lojavirtual/containers/Carrinho/index.js
+++ lojavirtual/containers/Carrinho/index.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+import actions from '../../redux/actions';
+
 import ListaDeProdutos from './ListaDeProdutos';
 import DadosDoCarrinho from './DadosDoCarrinho';
 
 class CarrinhoContainer extends Component {
+  componentDidMount() {
+    this.props.setCarrinho();
+  }
+
+  componentDidUpdate(prevProps) {
+    const { carrinho } = this.props;
+    if (
+      carrinho &&
+      carrinho[0] &&
+      carrinho[0].produto &&
+      !carrinho[0].produto._id
+    ) {
+      carrinho.forEach((item, idx) => {
+        this.props.fetchProdutoCarrinho(item.produto, idx);
+        this.props.fetchVariacoesCarrinho(item.variacao, item.produto, idx);
+      });
+    }
+  }
+
   render() {
     return (
       <div className="container Carrinho">
@@ -16,4 +38,8 @@ class CarrinhoContainer extends Component {
   }
 }
 
-export default CarrinhoContainer;
+const mapStateToProps = (state) => ({
+  carrinho: state.carrinho.carrinho,
+});
+
+export default connect(mapStateToProps, actions)(CarrinhoContainer);
