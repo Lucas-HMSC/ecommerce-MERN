11b7da32ceb5e61c766d8157cbf8b3360db5492d
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-03-21 14:54:41 -0300

    Integrating "Change Password"

--- lojavirtual/pages/area-cliente/alterar-senha.js
+++ lojavirtual/pages/area-cliente/alterar-senha.js
@@ -4,8 +4,21 @@ import Layout from '../../components/Layout';
 import Cabecalho from '../../containers/Cabecalho';
 import AlterarSenhaContainer from '../../containers/AreaDoCliente/AlterarSenha';
 import Rodape from '../../containers/Rodape';
+import { connect } from 'react-redux';
+import actions from '../../redux/actions';
+import callBaseData from '../../utils/callBaseData';
+import initialize from '../../utils/initialize';
 
-export default class AlterarSenha extends Component {
+class AlterarSenha extends Component {
+  static async getInitialProps(ctx) {
+    initialize(ctx);
+    return callBaseData([], ctx);
+  }
+
+  async componentDidMount() {
+    await this.props.getUser({ token: this.props.token });
+  }
+  
   render() {
     return (
       <Layout title="Alterar Senha | Loja TI - Melhores produtos de tecnologia">
@@ -16,3 +29,9 @@ export default class AlterarSenha extends Component {
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  token: state.auth.token,
+});
+
+export default connect(mapStateToProps, actions)(AlterarSenha);
