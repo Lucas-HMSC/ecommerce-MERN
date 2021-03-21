72cca2af38f3cfd7515b96d78049348b4d6a3e7d
lucas-hmsc <lucashms.carvalho@gmail.com>
2021-03-20 21:09:11 -0300

    Integrating Login

--- lojavirtual/containers/AreaDoCliente/Acesso/Login.js
+++ lojavirtual/containers/AreaDoCliente/Acesso/Login.js
@@ -1,15 +1,51 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import FormSimples from '../../../components/Inputs/FormSimples';
+import { API, versao } from '../../../config';
+import actions from '../../../redux/actions';
+
+import AlertGeral from '../../../components/Alert/Geral';
 
 class LoginContainer extends Component {
   state = {
     email: '',
     senha: '',
+    aviso: null,
+    erros: {},
   };
 
-  render() {
+  validate() {
     const { email, senha } = this.state;
+    const erros = {};
+
+    if (!email) erros.email = 'Preencha aqui com o seu email';
+    if (!senha) erros.senha = 'Preencha aqui com a sua senha';
+
+    this.setState({ erros, aviso: null });
+    return Object.keys(erros).length === 0;
+  }
+
+  handleSubmit() {
+    if (!this.validate()) return;
+    const { email, senha } = this.state;
+    this.props.autenticar({ email, password: senha }, false, (error) => {
+      if (error)
+        this.setState({
+          aviso: {
+            status: false,
+            message: error.message,
+          },
+        });
+      else this.setState({ aviso: null });
+    });
+  }
+
+  onChange = (field, value) =>
+    this.setState({ [field]: value }, () => this.validate());
+
+  render() {
+    const { email, senha, erros } = this.state;
     return (
       <div className="Login-Container">
         <h2 className="text-center">Minha Conta</h2>
@@ -18,19 +54,34 @@ class LoginContainer extends Component {
         <div className="form-login">
           <FormSimples
             value={email}
+            erro={erros.email}
             name="email"
             type="email"
             placeholder="Email"
+            onChange={() => this.onChange('email', e.target.value)}
           />
           <FormSimples
             value={senha}
+            erro={erros.senha}
             name="senha"
             type="password"
             placeholder="Senha"
+            onChange={() => this.onChange('senha', e.target.value)}
           />
+          <div className="flex flex-right">
+            <a href={`${API}/${versao}/api/usuarios/recuperar-senha`}>
+              <small>Esqueceu sua senha?</small>
+            </a>
+          </div>
           <br />
+          <AlertGeral aviso={this.state.aviso} />
           <div className="flex flex-center">
-            <button className="btn btn-primary">ENTRAR</button>
+            <button
+              className="btn btn-primary"
+              onClick={() => this.handleSubmit()}
+            >
+              ENTRAR
+            </button>
           </div>
           <br />
           <hr />
@@ -45,4 +96,4 @@ class LoginContainer extends Component {
   }
 }
 
-export default LoginContainer;
+export default connect(null, actions)(LoginContainer);
--- lojavirtual/pages/area-cliente/index.js
+++ lojavirtual/pages/area-cliente/index.js
@@ -5,7 +5,21 @@ import Cabecalho from '../containers/Cabecalho';
 import AreaDoCliente from '../containers/AreaDoCliente';
 import Rodape from '../containers/Rodape';
 
-export default class AreaDoCliente extends Component {
+import initialize from '../../utils/initialize';
+import callBaseData from '../../utils/callBaseData';
+import { connect } from 'react-redux';
+import actions from '../../redux/actions';
+
+class AreaDoCliente extends Component {
+  static async getInitialProps(ctx) {
+    initialize(ctx);
+    return callBaseData([], ctx);
+  }
+
+  async componentDidMount() {
+    await this.props.getUser({ token: this.props.token });
+  }
+
   render() {
     return (
       <Layout title="Loja TI - Melhores produtos de tecnologia">
@@ -16,3 +30,9 @@ export default class AreaDoCliente extends Component {
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  token: state.auth.token,
+});
+
+export default connect(mapStateToProps, actions)(AreaDoCliente);
