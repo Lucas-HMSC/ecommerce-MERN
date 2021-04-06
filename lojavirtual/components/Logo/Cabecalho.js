7425cdb6c04e2840ce0eb907e17e84aa83ed1a87
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-04-06 16:12:33 -0300

    Bugs Fixed

--- lojavirtual/components/Logo/Cabecalho.js
+++ lojavirtual/components/Logo/Cabecalho.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 
 const LogoCabecalho = () => (
-  <div className="flex-2 flex-center">
+  <div className="flex-2 flex flex-center">
     <Link href="/">
       <img src="/static/logo.png" alt="Logo" className="logo" width="90%" />
     </Link>
